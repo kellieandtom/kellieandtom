40fa0c3767c28dc651556bb35174a80220994a5d
lakellie <kellie.morgan.ur@gmail.com>
2016-02-21 23:00:18 -0500

    Update accomodations page with hotel information

--- accommodations.php
+++ accommodations.php
@@ -4,17 +4,54 @@
   $template_theme = 'wedding';
 ?>
 <?php include("inc/header.php"); ?>
-<h4>Holiday Inn - Buffalo Downtown</h4>
-<p>Rooms starting at $120</p>
-<p><strong>.3 miles</strong> from ceremony site</p>
 
-<h4>Best Western on the Avenue</h4>
-<p>Rooms starting at $130</p>
-<p><strong>.5 miles</strong> from ceremony site</p>
+<p>There are many great hotel options in the downtown Buffalo area. Here is a list of hotels at varying price points, 
+all extremely close to our wedding location! All of the hotels we've listed here are highly rated.</p>
 
-<h4>The Mansion on Delaware Avenue</h4>
-<p>Rooms starting at $300</p>
+<h4>Chain Hotels</h4>
 
-<h4>Hampton Inn & Suites</h4>
-<p>Rooms starting at $200</p>
+<h4><a href="http://hibuffalodowntown.com/">Holiday Inn - Buffalo Downtown</a></h4>
+<p>Rooms starting at $120 USD</p>
+<p><strong>1/2 mile</strong> from ceremony site, 2 miles from reception site</p>
+
+<h4><a href="http://book.bestwestern.com/bestwestern/US/NY/Buffalo-hotels/BEST-WESTERN-On-The-Avenue/Hotel-Overview.do?propertyCode=33035">Best Western on the Avenue</a></h4>
+<p>Rooms starting at $130 USD</p>
+<p><strong>1/2 mile</strong> from ceremony site, 2 miles from reception site</p>
+
+<h4><a href="http://hamptoninn3.hilton.com/en/hotels/new-york/hampton-inn-and-suites-buffalo-downtown-BUFDTHX/index.html">Hampton Inn & Suites - Buffalo Downtown</a></h4>
+<p>Rooms starting at $200 USD</p>
+<p>1 mile from ceremony site, 1 mile from reception site</p>
+
+<h4><a href="http://doubletree3.hilton.com/en/hotels/new-york/doubletree-club-by-hilton-hotel-buffalo-downtown-BUFNYDT/index.html">Doubletree Club by Hilton - Buffalo Downtown</a></h4>
+<p>Rooms starting at $135 USD</p>
+<p>1 mile from ceremony site, 3 miles from reception site</p>
+
+<h4><a href="http://embassysuites3.hilton.com/en/hotels/new-york/embassy-suites-by-hilton-buffalo-BUFESES/index.html">Embassy Suites Buffalo</a>a</h4>
+<p>Rooms starting at $185 USD</p>
+<p>1 mile from ceremony site, 1 mile from reception site</p>
+
+<h4><a href="http://www.marriott.com/hotels/travel/bufbd-buffalo-downtown-marriott/">Buffalo Mariott Harborcenter</a></h4>
+<p>Rooms starting at $230</p>
+<p>2 miles from ceremony site, 1/2 mile from reception site</p>
+
+<h4>Unique/Luxury/Boutique Hotels</h4>
+
+<h4><a href="http://www.mansionondelaware.com/">The Mansion on Delaware</a></h4>
+<p>Rooms starting at $275 USD</p>
+<p>1/2 mile from ceremony site, 2 miles from reception site</p>
+
+<h4><a href="http://www.innbuffalo.com">InnBuffalo</a></h4>
+<p>Rooms starting at $200 USD</p>
+<p>1 mile from ceremony site, 4 miles from reception site</p>
+
+<h4><a href="http://buffalobrewerydistrict.com/lofts/rooms">Lofts on Pearl</a></h4>
+<p>Rooms starting at $200 USD</p>
+<p>2 miles from ceremony site, 1 mile from reception site</p>
+
+<h4><a href="http://buffalobrewerydistrict.com/lafayette/home">Hotel @ The Lafayette</h4></a>
+<p>Rooms starting at $200 USD</p>
+<p>2 miles from ceremony site, 1 mile from reception site</p>
+
+
+
 <?php include("inc/footer.php"); ?>
