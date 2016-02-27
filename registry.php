de68d94087593070acbdfea1ee78801de8e3c2d8
lakellie <kellie.morgan.ur@gmail.com>
2016-02-27 16:12:29 -0500

    Add content to Getting Here and Registry pages

--- registry.php
+++ registry.php
@@ -4,8 +4,9 @@
   $template_theme = 'wedding';
 ?>
 <?php include("inc/header.php"); ?>
-<ul>
-  <li>blurb about registry
-  <li>links to where we're registered
-</ul>
+<h4>Registry</h4>
+
+<p>We have registered at Hudson's Bay. To access our registry, <a href="http://giftregistry.hbc.com/grworks/prj/generic_online/jsp/common/marcole_grw.jsp?rid=4">click here</a> to visit the Hudson's Bay registry site and locate our registry by searching for either of our names. Purchases can be made online through our registry page, or they can be made in-store.</p>
+
+<p>To our American guests: while Hudson's Bay is a Canadian department store, if you would like to purchase a gift from our registry, we have confirmed that American cards can be used for online checkout. Simply select your gift from our online page and check out as normal. Keep in mind that the prices you see are in Canadian dollars, and you will be charged U.S. dollars if you use an American card. This means you will be paying much less than the amount listed in CAD, due to the current exchange rate.</p>
 <?php include("inc/footer.php"); ?>
