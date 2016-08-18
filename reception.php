656b68b17b7fdee01d886213b42fe1c55023f809
lakellie <kellie.morgan.ur@gmail.com>
2016-08-17 22:54:52 -0400

    Update reception.php

--- reception.php
+++ reception.php
@@ -5,10 +5,12 @@
 ?>
 <?php include("inc/header.php"); ?>
 <h2>The Reception</h2>
-<p><strong>Templeton Landing</strong></p>
+<p>Cocktail hour beginning at 4:00</p>
+<p>Templeton Landing</p>
 <p>2 Templeton Terrace</p>
-<p>Buffalo, NY</p>
-<img src="img/templetonlanding-image.jpg" class="float-none">
+<p>Buffalo, NY</p><br>
+<img src="templeton-2.jpg" class="float-none">
 <h3>Parking</h3>
 <p>There will be valet parking for all guests at the reception. Pull up to the archway in front of the restaurant and the valet will take care of the rest.</p>
+<br><img src="map-templeton.jpg" class="float-none">
 <?php include("inc/footer.php"); ?>
